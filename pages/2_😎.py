f423dfdbd7e580565112072faedf49e747d3e2cd
Elseb18 <144059233+Elseb18@users.noreply.github.com>
2023-10-04 13:23:25 -0300

    2_😎.py

--- pages/2_😎.py
+++ pages/2_😎.py
@@ -2,21 +2,19 @@
 import streamlit as st
 from PIL import Image
 
-
+#en esta parte del codigo se genera el titulo y las opciones del usuario
 st.title("¿Quieres saber que edad tiene tu mascota? (años humanos)")
 mascota = st.radio(
         "Escoge tu mascota👇",
         ["Perro", "Gato", "No tengo :("])
 
+#en esta parte del codigo se hace que al saber la opcion del usuario nos muestra ciertos resultados
 if mascota == "Perro":
     image = Image.open("ilustracion-icono-vector-dibujos-animados-hueso-mordedura-perro-pug-lindo-naturaleza-animal-icono-co.jpg")
     st.image(image, caption='U・ᴥ・U')
     if st.button("confirma guau"):
         tabla = Image.open("imagen_2023-09-25_153528724.jpg")
         st.image(tabla, caption='tabla')
-
-
-    
 
 elif mascota == "Gato":
     image = Image.open("ilustracion-icono-vector-dibujos-animados-lindo-gato-sentado-concepto-icono-naturaleza-animal-aislad.jpg")
