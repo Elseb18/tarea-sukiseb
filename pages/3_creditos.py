c05175dfef7ad8c3dc4f1c19592bfecf029379ba
Elseb18 <144059233+Elseb18@users.noreply.github.com>
2023-10-04 13:22:21 -0300

    3_creditos.py

--- pages/3_creditos.py
+++ pages/3_creditos.py
@@ -1,12 +1,13 @@
+#en esta parte del codigo de importan las librerias
 import streamlit as st
 from PIL import Image
 
-
+#en esta parte del codigo muestra texto en pantalla
 st.title("Esta app fue creada por:")
 st.title("Fernanda Barbé y Sebastián Castillo")
 st.title("DEL 4TO BLUE.")
 
-
+#con esta parte se muestran las imagenes
 image = Image.open("download20231001145808.jpg")
 
 
@@ -14,7 +15,7 @@ image = Image.open("download20231001145808.jpg")
 image = Image.open("download20231001150602.jpg")
 
 
-
+# con esta parte ordenamos las imagenes en columnas
 col1, col2, = st.columns(2)
 
 with col1:
